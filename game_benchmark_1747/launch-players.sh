4fbca829063c6c3cd285587d88efe85443f55b94
Francis Deslauriers <francis.deslauriers@efficios.com>
2014-10-24 13:25:14 -0400

    Add host argument

--- game_benchmark_1747/launch-players.sh
+++ game_benchmark_1747/launch-players.sh
@@ -1,7 +1,7 @@
 host="127.0.0.1"
 port="12345"
-echo "Launching $1 players to connect at $host:$port" 
-for i in $(seq 1 $1);
+echo "Launching $2 players to connect at $1:$port" 
+for i in $(seq 1 $2);
 do
-  ./client $host":"$port > /dev/null 2>&1 &
+  ./client $1":"$port > /dev/null 2>&1 &
 done
