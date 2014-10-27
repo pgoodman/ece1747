3cb5c41ab9f1c92d009ea35454e21d22c82071b1
Francis Deslauriers <francis.deslauriers@efficios.com>
2014-10-27 17:24:47 -0400

    Add generate graphs

---
+++ game_benchmark_1747/generate_graphs.sh
@@ -0,0 +1,4 @@
+gnuplot -e "out_file='$1_num_req.png'" -e "duration=2000" -e "qbegin=300" -e "qend=1300" plot_num_req.p
+gnuplot -e "out_file='$1_num_updates.png'" -e "duration=2000" -e "qbegin=300" -e "qend=1300" plot_num_updates.p
+gnuplot -e "out_file='$1_num_req.png'" -e "duration=2000" -e "qbegin=300" -e "qend=1300" plot_proc_time.p
+#gnuplot -e "out_file='$1_num_req.png'" -e "duration=2000" -e "qbegin=300" -e "qend=1300" plot_.p
