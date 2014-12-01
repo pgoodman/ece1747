cbcaa31538ac5420c49150f97827222e1f597faa
Peter Goodman <pag@c151.csl.toronto.edu>
2014-12-01 17:23:51 -0500

    Fixed the cgroups stuff.

--- game_benchmark_1747/create_cgroups.sh
+++ game_benchmark_1747/create_cgroups.sh
@@ -2,12 +2,15 @@
 #!/bin/bash
 set -x
 
-USERNAME= $SUDO_USER
-SERVER_CPUS=2
-CLIENT_CPUS=2
+USERNAME=$SUDO_USER
+SERVER_CPUS=4
+CLIENT_CPUS=60
 #cgroup for server
-sudo cgcreate -t $USERNAME -a $USERNAME -g cpuset:simmud-server
+sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-server
 sudo cgset -r cpuset.cpus="0-$((SERVER_CPUS-1))" simmud-server
 #cgroup for clients
-sudo cgcreate -t $USERNAME -a $USERNAME -g cpuset:simmud-clients
+sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-clients
 sudo cgset -r cpuset.cpus="$((SERVER_CPUS))-$((SERVER_CPUS+CLIENT_CPUS-1))" simmud-clients
+cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-server/cpuset.mems
+cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-clients/cpuset.mems
+
