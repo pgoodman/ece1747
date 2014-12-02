4f4fc5066d417da1e12b10022585bdde632e8e3c
Francis Deslauriers <francis.deslauriers@mail.utoronto.ca>
2014-12-02 18:56:30 -0500

    Add ece1747 group to cgroup creation script

--- game_benchmark_1747/create_cgroups.sh
+++ game_benchmark_1747/create_cgroups.sh
@@ -3,17 +3,18 @@
 set -x
 
 USERNAME=$SUDO_USER
+GROUPNAME="ece1747"
 NUM_CPUS=63
 SERVER_CPUS=$1
 CLIENT_CPUS=$((NUM_CPUS-SERVER_CPUS))
 #cgroup for server
-sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-server
+sudo cgcreate -t $USERNAME:$GROUPNAME -a $USERNAME:$GROUPNAME -g cpuset:simmud-server
 sudo cgset -r cpuset.cpus="0-$((SERVER_CPUS-1))" simmud-server
 #cgroup for clients
-sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-clients
+sudo cgcreate -t $USERNAME:$GROUPNAME -a $USERNAME:$GROUPNAME -g cpuset:simmud-clients
 sudo cgset -r cpuset.cpus="$((SERVER_CPUS))-$((SERVER_CPUS+CLIENT_CPUS-1))" simmud-clients
 
-sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-lttng
+sudo cgcreate -t $USERNAME:$GROUPNAME -a $USERNAME:$GROUPNAME -g cpuset:simmud-lttng
 sudo cgset -r cpuset.cpus="63" simmud-lttng
 cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-server/cpuset.mems
 cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-clients/cpuset.mems
