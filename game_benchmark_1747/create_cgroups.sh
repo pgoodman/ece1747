2f3d19a70df518674d78de78151e613207dd9481
Peter Goodman <peter.goodman@gmail.com>
2014-12-02 18:01:38 -0500

    minor changes

--- game_benchmark_1747/create_cgroups.sh
+++ game_benchmark_1747/create_cgroups.sh
@@ -3,7 +3,7 @@
 set -x
 
 USERNAME=$SUDO_USER
-NUM_CPUS=64
+NUM_CPUS=63
 SERVER_CPUS=$1
 CLIENT_CPUS=$((NUM_CPUS-SERVER_CPUS))
 #cgroup for server
@@ -12,6 +12,10 @@ sudo cgset -r cpuset.cpus="0-$((SERVER_CPUS-1))" simmud-server
 #cgroup for clients
 sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-clients
 sudo cgset -r cpuset.cpus="$((SERVER_CPUS))-$((SERVER_CPUS+CLIENT_CPUS-1))" simmud-clients
+
+sudo cgcreate -t $USERNAME:$USERNAME -a $USERNAME:$USERNAME -g cpuset:simmud-lttng
+sudo cgset -r cpuset.cpus="63" simmud-lttng
 cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-server/cpuset.mems
 cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-clients/cpuset.mems
+cat /sys/fs/cgroup/cpuset/cpuset.mems > /sys/fs/cgroup/cpuset/simmud-lttng/cpuset.mems
 
